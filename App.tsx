0fce6ec42cef6e189fb80f1eeaec5fadd995ce05
sarthakpranesh <sarthakpranesh08@gmail.com>
2021-08-20 09:00:29 +0530

    Added the basic camera features
    Signed-off-by: sarthakpranesh <sarthakpranesh08@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+
+const {width, height} = Dimensions.get('screen');
+
+let camera: Camera;
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(false);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  const takePictureAsync = async () => {
+    if (camera === null) {
+      return;
+    }
+
+    const photo = await camera.takePictureAsync();
+    console.log(photo);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type} ref={(r) => camera = r}>
+        <TouchableOpacity style={styles.mainCameraButton} onPress={takePictureAsync} />
+      </Camera>
     </View>
   );
 }
@@ -14,8 +46,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'pink',
   },
+  camera: {
+    flex: 1,
+    height,
+    width,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",   
+  },
+  mainCameraButton: {
+    width: 60,
+    height: 60,
+    borderRadius: 20,
+    backgroundColor: `rgba(255, 255, 255, 0.8)`,
+    position: 'absolute',
+    bottom: 10,
+  }
 });
